2cb63df15a8e6ffae2b7ed8a29d396b56e2fa490
tperneta <taperneta@gmail.com>
2021-02-18 13:16:01 +0100

    update app with header containing logo and links to shop and contact

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const HatsPage = () => (
   <div>
@@ -16,6 +17,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
