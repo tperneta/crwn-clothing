11f44872ecf1422b993245e4a0316c4f709d04e2
tperneta <taperneta@gmail.com>
2021-02-22 12:46:20 +0100

    created checkout page and checkout-item components and update cart selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,6 +7,10 @@ export const selectCartItems = createSelector(
 	(cart) => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+	selectCart,
+	cart => cart.hidden
+)
 
 export const selectCartItemsCount = createSelector(
 	[selectCartItems],
@@ -17,3 +21,13 @@ export const selectCartItemsCount = createSelector(
 				0
 		)
 );
+
+export const selectCartTotal = createSelector(
+	selectCartItems,
+	cartItems =>
+		cartItems.reduce(
+			(accumulatedQuantity, cartItem) => 
+				accumulatedQuantity + cartItem.quantity * cartItem.price, 
+				0
+		)
+)
